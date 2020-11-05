ded883ba8196a1ff6cf467240a625a609d8015e2
Vishal-SlangLabs <vishal@slanglabs.in>
2020-11-05 13:20:48 +0530

    UI updates to the order/booking item + added another check for status and switch the UI accordingly

--- app/src/main/java/com/example/sampletravelapp/UI/ViewHolder/OrderItemViewHolder.java
+++ app/src/main/java/com/example/sampletravelapp/UI/ViewHolder/OrderItemViewHolder.java
@@ -7,8 +7,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sampletravelapp.Model.BusAttributes;
 import com.example.sampletravelapp.Model.JourneyBusPlace;
+import com.example.sampletravelapp.Model.JourneyBusPlaceOrder;
 import com.example.sampletravelapp.Model.OrderItem;
+import com.example.sampletravelapp.Model.OrderStatus;
 import com.example.sampletravelapp.R;
 import com.example.sampletravelapp.UI.ItemClickListener;
 
@@ -39,43 +42,54 @@ public class OrderItemViewHolder extends RecyclerView.ViewHolder {
         itemView.setOnClickListener(view -> itemClickListener.itemClicked(getAdapterPosition()));
     }
 
-    public void setData(OrderItem orderItem) {
-        JourneyBusPlace journeyBusPlace = orderItem.journeyBusPlace;
+    public void setData(JourneyBusPlaceOrder orderItem) {
+        JourneyBusPlace journeyBusPlace = orderItem.journey;
         String dateString = new SimpleDateFormat("EEE, dd MMM yyyy").
-                format(orderItem.journeyBusPlace.journey.date);
+                format(orderItem.order.journeyDate);
         journeyDateInformation.setText(dateString);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm", Locale.ENGLISH);
-        String startTime = String.format(Locale.ENGLISH,"%02d:%02d",
-                journeyBusPlace.journey.startHours, journeyBusPlace.journey.startMinutes);
-        Date startDateTime = null;
-        try {
-            startDateTime = dateFormat.parse(startTime);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Date expireDate = new Date(startDateTime.getTime() + journeyBusPlace.journey.duration*1000);
-        SimpleDateFormat  dateformatFinal = new SimpleDateFormat("hh:mm aa",Locale.ENGLISH);
-        String strDate = dateformatFinal.format(startDateTime);
-        String endDate = dateformatFinal.format(expireDate);
+        SimpleDateFormat dateformatFinal = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH);
+        String strDate = dateformatFinal.format(journeyBusPlace.journey.startTime);
+        String endDate = dateformatFinal.format(journeyBusPlace.journey.endTime);
         String journeyTimeInfo = strDate + " - " + endDate;
         journeyTimeInformation.setText(journeyTimeInfo);
         busName.setText(journeyBusPlace.bus.name);
         busType.setText(journeyBusPlace.bus.type);
-        journeyPrice.setText(String.format(Locale.ENGLISH,"Rs %d", journeyBusPlace.journey.price));
-        journeyDuration.setText(String.format(Locale.ENGLISH,"%02d hours %02d min",
+        for (BusAttributes attributes : journeyBusPlace.busAttributesList) {
+            busType.setText(String.format(Locale.ENGLISH, "%s %s", busType.getText().toString(), attributes.travelClass));
+        }
+        journeyPrice.setText(String.format(Locale.ENGLISH, "Rs %d", journeyBusPlace.journey.price));
+        journeyDuration.setText(String.format(Locale.ENGLISH, "%02d hours %02d min",
                 (journeyBusPlace.journey.duration / 3600),
                 (journeyBusPlace.journey.duration % 3600) / 60));
-        if(orderItem.active) {
+        if (new Date().getTime() > orderItem.order.journeyDate.getTime()) {
             orderStateTextView.setVisibility(View.VISIBLE);
-            orderStateTextView.setText("Active");
-            orderStateTextView.setBackgroundColor(Color.GREEN);
+            orderStateTextView.setText("Completed");
+            orderStateTextView.setBackgroundColor(Color.GRAY);
             orderStateTextView.setTextColor(Color.BLACK);
-        }
-        else {
-            orderStateTextView.setVisibility(View.VISIBLE);
-            orderStateTextView.setText("Cancelled");
-            orderStateTextView.setBackgroundColor(Color.RED);
-            orderStateTextView.setTextColor(Color.WHITE);
+        } else {
+            switch (orderItem.order.active) {
+                case OrderStatus
+                        .ON_SCHEDULE:
+                    orderStateTextView.setVisibility(View.VISIBLE);
+                    orderStateTextView.setText("On Schedule");
+                    orderStateTextView.setBackgroundColor(Color.GREEN);
+                    orderStateTextView.setTextColor(Color.BLACK);
+                    break;
+                case OrderStatus
+                        .DELAYED:
+                    orderStateTextView.setVisibility(View.VISIBLE);
+                    orderStateTextView.setText("Delayed");
+                    orderStateTextView.setBackgroundColor(Color.BLACK);
+                    orderStateTextView.setTextColor(Color.WHITE);
+                    break;
+                case OrderStatus
+                        .CANCELED:
+                    orderStateTextView.setVisibility(View.VISIBLE);
+                    orderStateTextView.setText("Cancelled");
+                    orderStateTextView.setBackgroundColor(Color.RED);
+                    orderStateTextView.setTextColor(Color.WHITE);
+                    break;
+            }
         }
     }
 }
