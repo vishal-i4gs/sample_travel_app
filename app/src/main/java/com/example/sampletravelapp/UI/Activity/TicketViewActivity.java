384660aad33a050c562bbc3ec140bd553cc3c95d
Vishal-SlangLabs <vishal@slanglabs.in>
2020-11-06 00:01:08 +0530

    Moved common implementation/setup of routeDetails and ticketView to a baseDetailsActivity class

--- app/src/main/java/com/example/sampletravelapp/UI/Activity/TicketViewActivity.java
+++ app/src/main/java/com/example/sampletravelapp/UI/Activity/TicketViewActivity.java
@@ -3,47 +3,18 @@ package com.example.sampletravelapp.UI.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sampletravelapp.Model.BusAttributes;
 import com.example.sampletravelapp.Model.OrderStatus;
-import com.example.sampletravelapp.R;
-import com.example.sampletravelapp.UI.ViewModel.AppViewModel;
 
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-public class TicketViewActivity extends AppCompatActivity {
-
-    private AppViewModel appViewModel;
-    private final SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy, hh:mm a", Locale.ENGLISH);
+public class TicketViewActivity extends BaseDetailsActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ticket_view);
-
-        ImageView backButtonView = findViewById(R.id.back_button);
-        backButtonView.setOnClickListener(view -> finish());
-
-        TextView orderTitle = findViewById(R.id.order_title);
-        TextView journeyTime = findViewById(R.id.order_journey_date);
-        TextView journeyStartLocation = findViewById(R.id.order_journey_start_location);
-        TextView journeyEndLocation = findViewById(R.id.order_journey_end_location);
-        TextView journeyTotalDuration = findViewById(R.id.order_journey_total_duration);
-        TextView journeyBusTravelsName = findViewById(R.id.order_journey_travels_name);
-        TextView journeyBusAttributes = findViewById(R.id.order_journey_bus_type);
-        TextView orderJourneyStatus = findViewById(R.id.order_journey_status);
-
-        View orderCancelView = findViewById(R.id.order_cancel_button);
-
         String orderId = getIntent().getStringExtra("orderItemId");
-        appViewModel = new ViewModelProvider(this).get(
-                AppViewModel.class);
         appViewModel.getOrderItem(orderId).observe(this, orderItem -> {
             orderCancelView.setOnClickListener(view -> {
                 appViewModel.removeOrderItem(orderItem.order);
@@ -81,8 +52,6 @@ public class TicketViewActivity extends AppCompatActivity {
                     break;
             }
         });
-
-
     }
 
 
