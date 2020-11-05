2e799838c2ecb95d839d4b5471339464f8abb706
Vishal-SlangLabs <vishal@slanglabs.in>
2020-11-05 13:22:26 +0530

    Updated search activity with updated sort params + interaction with the filter dialog fragment

--- app/src/main/java/com/example/sampletravelapp/UI/Activity/SearchBusActivity.java
+++ app/src/main/java/com/example/sampletravelapp/UI/Activity/SearchBusActivity.java
@@ -1,50 +1,44 @@
 package com.example.sampletravelapp.UI.Activity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.sampletravelapp.Model.Bus;
 import com.example.sampletravelapp.Model.JourneyBusPlace;
 import com.example.sampletravelapp.Model.OrderBy;
 import com.example.sampletravelapp.Model.OrderItem;
+import com.example.sampletravelapp.Model.OrderStatus;
 import com.example.sampletravelapp.Model.Place;
 import com.example.sampletravelapp.R;
 import com.example.sampletravelapp.UI.Adapters.JourneyListAdapter;
+import com.example.sampletravelapp.UI.Fragment.FilterDialogFragment;
 import com.example.sampletravelapp.UI.ItemClickListener;
 import com.example.sampletravelapp.UI.ViewModel.AppViewModel;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
 
 public class SearchBusActivity extends AppCompatActivity {
 
     private AppViewModel appViewModel;
     private JourneyListAdapter listAdapter;
-    private List<String> busFilters = new ArrayList<>();
-    private List<Boolean> selectedBusFilters = new ArrayList<>();
-    private OrderBy orderBy = OrderBy.RATING;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_search_bus);
 
         ImageView backButtonView = findViewById(R.id.back_button);
@@ -59,6 +53,7 @@ public class SearchBusActivity extends AppCompatActivity {
         RecyclerView listItemView = findViewById(R.id.journey_list_recycler_view);
         Place startLocation = (Place) getIntent().getSerializableExtra("startLoc");
         Place endLocation = (Place) getIntent().getSerializableExtra("endLoc");
+
         long dateInt = getIntent().getLongExtra("date", 0);
         String dateString = new SimpleDateFormat("dd - MMM - yyyy | EEEE").format(new Date(dateInt));
         dateTextField.setText(dateString);
@@ -68,6 +63,9 @@ public class SearchBusActivity extends AppCompatActivity {
 
         appViewModel = new ViewModelProvider(this).get(
                 AppViewModel.class);
+        if(savedInstanceState == null) {
+            appViewModel.setBusFilterOptions(getIntent().getParcelableExtra("busFilterOptions"));
+        }
         appViewModel.getSearchForStartStopLocationMediator().observe(this, journeys
                 -> {
             Log.d("here", String.valueOf(journeys));
@@ -82,40 +80,45 @@ public class SearchBusActivity extends AppCompatActivity {
             @Override
             public void itemClicked(JourneyBusPlace journeyBusPlace) {
                 Log.d("here", "here");
+                Date journeyDate = new Date(dateInt);
+                Calendar journeyCal = Calendar.getInstance();
+                journeyCal.setTime(journeyDate);
+                Calendar tripCal = Calendar.getInstance();
+                tripCal.setTime(journeyBusPlace.journey.startTime);
+                journeyCal.set(Calendar.HOUR_OF_DAY, tripCal.get(Calendar.HOUR_OF_DAY));
+                journeyCal.set(Calendar.MINUTE, tripCal.get(Calendar.MINUTE));
+                journeyCal.set(Calendar.SECOND, tripCal.get(Calendar.SECOND));
+                Date finalJourneyDate = journeyCal.getTime();
+                if(finalJourneyDate.getTime() < new Date().getTime()) {
+                    Toast.makeText(SearchBusActivity.this,"Cannot book for this time slot",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 new AlertDialog.Builder(SearchBusActivity.this)
                         .setTitle("Book ticket")
                         .setMessage("Are you sure you want to book this?")
                         .setPositiveButton(android.R.string.yes, (dialog, which) -> {
-                            journeyBusPlace.journey.date = new Date(dateInt);
                             OrderItem orderItem = new OrderItem();
                             orderItem.orderId = UUID.randomUUID().toString();
-                            orderItem.active = true;
+                            orderItem.active = OrderStatus.ON_SCHEDULE;
                             orderItem.orderTime = new Date();
-                            orderItem.journeyBusPlace = journeyBusPlace;
+                            orderItem.journeyDate = finalJourneyDate;
+                            orderItem.journeyId = journeyBusPlace.journey.journeyId;
                             appViewModel.addOrderItem(orderItem);
                         })
                         .setNegativeButton(android.R.string.no, null)
                         .show();
             }
         });
-        appViewModel.getAllBuses().observe(this, new Observer<List<Bus>>() {
-            @Override
-            public void onChanged(List<Bus> buses) {
-                for (Bus bus : buses) {
-                    if (!busFilters.contains(bus.type)) {
-                        busFilters.add(bus.type);
-                        selectedBusFilters.add(true);
-                    }
-                }
-                List<String> finalBusFilters = new ArrayList<>();
-                for (int i = 0; i < selectedBusFilters.size(); i++) {
-                    if (selectedBusFilters.get(i)) {
-                        finalBusFilters.add(busFilters.get(i));
-                    }
-                }
-                appViewModel.getItemsForNameOrderBy(startLocation.id, endLocation.id, finalBusFilters, orderBy);
-            }
-        });
+        appViewModel.getAllBusAttributes().observe(this, filters ->
+                appViewModel.getItemsForNameOrderBy(startLocation.id,
+                endLocation.id,
+                        appViewModel.getBusFilterOptions().getBusFilters(),
+                        appViewModel.getBusFilterOptions().getBusType(),
+                        appViewModel.getBusFilterOptions().getBusOperators(),
+                        appViewModel.getBusFilterOptions().getBusDepartureTimeRange(),
+                        appViewModel.getBusFilterOptions().getBusArrivalTimeRange(),
+                appViewModel.getOrderBy()));
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         listItemView.setLayoutManager(layoutManager);
         listItemView.setItemAnimator(null);
@@ -123,59 +126,68 @@ public class SearchBusActivity extends AppCompatActivity {
 
         ImageView busFilterButton = findViewById(R.id.bus_filter_button);
         busFilterButton.setOnClickListener((View.OnClickListener) view -> {
-            final boolean[] selectedBusFiltersArray = new boolean[selectedBusFilters.size()];
-            for (int i = 0; i < selectedBusFilters.size(); i++) {
-                selectedBusFiltersArray[i] = selectedBusFilters.get(i);
-            }
-            final AlertDialog.Builder mBuilder = new AlertDialog.Builder(SearchBusActivity.this);
-            mBuilder.setTitle("Select preferred filter:");
-            mBuilder.setMultiChoiceItems(busFilters.toArray(new String[0]),
-                    selectedBusFiltersArray, (DialogInterface.OnMultiChoiceClickListener)
-                            (dialog, position, isChecked) -> {
-                                selectedBusFilters.set(position, isChecked);
-                            });
-            mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    List<String> finalBusFilters = new ArrayList<>();
-                    for (int i = 0; i < selectedBusFilters.size(); i++) {
-                        if (selectedBusFilters.get(i)) {
-                            finalBusFilters.add(busFilters.get(i));
-                        }
-                    }
-                    appViewModel.getItemsForNameOrderBy(startLocation.id, endLocation.id, finalBusFilters, orderBy);
-                }
-            });
-            AlertDialog mDialog = mBuilder.create();
-            mDialog.show();
+            FilterDialogFragment newFragment = FilterDialogFragment.newInstance(appViewModel.getBusFilterOptions());
+            newFragment.viewItemListener = busFilterOptions -> {
+                appViewModel.setBusFilterOptions(busFilterOptions);
+                appViewModel.getItemsForNameOrderBy(startLocation.id,
+                        endLocation.id,
+                        busFilterOptions.getBusFilters(),
+                        busFilterOptions.getBusType(),
+                        busFilterOptions.getBusOperators(),
+                        busFilterOptions.getBusDepartureTimeRange(),
+                        busFilterOptions.getBusArrivalTimeRange(),
+                        appViewModel.getOrderBy());
+            };
+            newFragment.show(getSupportFragmentManager(), "FilterAndSortDialogFragment");
         });
 
         ImageView busSortButton = findViewById(R.id.bus_sort_button);
         busSortButton.setOnClickListener((View.OnClickListener) view -> {
-            final String[] ordering = new String[2];
+            final String[] ordering = new String[4];
             int selectedIndxForOrdering = 0;
             ordering[0] = "Rating";
             ordering[1] = "Price";
-            if (orderBy == OrderBy.PRICE) {
-                selectedIndxForOrdering = 1;
+            ordering[2] = "Deparature Time";
+            ordering[3] = "Travel Duration";
+            switch (appViewModel.getOrderBy()) {
+                case PRICE:
+                    selectedIndxForOrdering = 1;
+                    break;
+                case DEPARTURE_TIME:
+                    selectedIndxForOrdering = 2;
+                    break;
+                case TRAVEL_DURATION:
+                    selectedIndxForOrdering = 3;
+                    break;
+                default:
+                    selectedIndxForOrdering = 0;
             }
             final AlertDialog.Builder mBuilder = new AlertDialog.Builder(SearchBusActivity.this);
-            mBuilder.setTitle("Select preferred filter:");
+            mBuilder.setTitle("Select preferred sort:");
             mBuilder.setSingleChoiceItems(ordering, selectedIndxForOrdering, (dialogInterface, i) -> {
-                if (i == 0) {
-                    orderBy = OrderBy.RATING;
-                } else {
-                    orderBy = OrderBy.PRICE;
+                switch (i) {
+                    case 1:
+                        appViewModel.setOrderBy(OrderBy.PRICE);
+                        break;
+                    case 2:
+                        appViewModel.setOrderBy(OrderBy.DEPARTURE_TIME);
+                        break;
+                    case 3:
+                        appViewModel.setOrderBy(OrderBy.TRAVEL_DURATION);
+                        break;
+                    default:
+                        appViewModel.setOrderBy(OrderBy.RATING);
                 }
             });
             mBuilder.setPositiveButton("OK", (dialog, which) -> {
-                List<String> finalBusFilters = new ArrayList<>();
-                for (int i = 0; i < selectedBusFilters.size(); i++) {
-                    if (selectedBusFilters.get(i)) {
-                        finalBusFilters.add(busFilters.get(i));
-                    }
-                }
-                appViewModel.getItemsForNameOrderBy(startLocation.id, endLocation.id, finalBusFilters, orderBy);
+                appViewModel.getItemsForNameOrderBy(startLocation.id,
+                        endLocation.id,
+                        appViewModel.getBusFilterOptions().getBusFilters(),
+                        appViewModel.getBusFilterOptions().getBusType(),
+                        appViewModel.getBusFilterOptions().getBusOperators(),
+                        appViewModel.getBusFilterOptions().getBusDepartureTimeRange(),
+                        appViewModel.getBusFilterOptions().getBusArrivalTimeRange(),
+                        appViewModel.getOrderBy());
             });
             AlertDialog mDialog = mBuilder.create();
             mDialog.show();
