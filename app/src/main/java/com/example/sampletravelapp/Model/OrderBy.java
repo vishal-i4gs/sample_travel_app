93826de28301b00ce04a14d974db913cea0a1d4c
Vishal-SlangLabs <vishal@slanglabs.in>
2020-11-05 16:15:33 +0530

    Modified OrderBy to Int interface and added sort for relevance

--- app/src/main/java/com/example/sampletravelapp/Model/OrderBy.java
+++ app/src/main/java/com/example/sampletravelapp/Model/OrderBy.java
@@ -1,5 +1,17 @@
 package com.example.sampletravelapp.Model;
 
-public enum OrderBy {
-    RATING, PRICE, DEPARTURE_TIME, TRAVEL_DURATION
+import androidx.annotation.IntDef;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@IntDef({OrderBy.RELEVANCE, OrderBy.RATING, OrderBy.PRICE, OrderBy.DEPARTURE_TIME, OrderBy.TRAVEL_DURATION})
+@Retention(RetentionPolicy.SOURCE)
+public @interface OrderBy {
+    int RELEVANCE = 0;
+    int RATING = 1;
+    int PRICE = 2;
+    int DEPARTURE_TIME = 3;
+    int TRAVEL_DURATION = 4;
 }
+
