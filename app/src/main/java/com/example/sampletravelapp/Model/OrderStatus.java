373113329342ff85803d1ff743f67ebc93d7b1da
Vishal-SlangLabs <vishal@slanglabs.in>
2020-11-06 13:18:19 +0530

    Modified orderStatus field with the right accepted values

--- app/src/main/java/com/example/sampletravelapp/Model/OrderStatus.java
+++ app/src/main/java/com/example/sampletravelapp/Model/OrderStatus.java
@@ -7,12 +7,10 @@ import java.lang.annotation.RetentionPolicy;
 
 import static com.example.sampletravelapp.Model.OrderStatus.CANCELED;
 import static com.example.sampletravelapp.Model.OrderStatus.ON_SCHEDULE;
-import static com.example.sampletravelapp.Model.OrderStatus.DELAYED;
 
-@IntDef({CANCELED, ON_SCHEDULE, DELAYED})
+@IntDef({CANCELED, ON_SCHEDULE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface OrderStatus {
     int CANCELED = 0;
     int ON_SCHEDULE = 1;
-    int DELAYED = 2;
 }
